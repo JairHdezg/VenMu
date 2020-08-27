52e59e258cabdfc547bec4f6b2b0669bbc95fc9a
Harrison Hua <harrisonhhua@gmail.com>
2020-08-27 13:26:28 -0500

    star rating

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,13 @@ import Swiper from 'swiper/bundle';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox'
+import { initStarRating } from '../plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initStarRating();
 
 });
 
