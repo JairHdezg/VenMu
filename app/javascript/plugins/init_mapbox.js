4582b815b41613b629a88427080069fbb3e15022
Jair Hernandez <jairhdzgaytan@gmail.com>
2020-09-01 19:35:20 -0500

    sq

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,29 +8,62 @@ const initMapbox = () => {
   //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   // };
+  function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+  }
+
+  function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+  }
+
+  const params = getSearchParameters();
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/bright-v8'
+      style: 'mapbox://styles/mapbox/bright-v8',
+      center: [params['lon'], params['lat']],
+      zoom: 8
     });
-    
+
     var geolocate = new mapboxgl.GeolocateControl({
       positionOptions: {
-      enableHighAccuracy: true
+        enableHighAccuracy: false
       },
       trackUserLocation: true,
+      showAccuracyCircle: false,
     });
 
-    // Add the control to the map.
-    map.addControl(geolocate, 'top-left');
-    map.on('load', function() {
-      geolocate.trigger();
-    });
+    if (params['gl'] == 'y') {
+      // Add the control to the map.
+      map.addControl(geolocate, 'top-left');
+      map.on('load', function() {
+        geolocate.trigger();
+        map.flyTo({
+        center: [params['lon'], params['lat']],
+        zoom: 8
+      });
+      });
 
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl, placeholder: "Search location!" }), "top-right");
+    }
+
+    // map.flyTo({
+    //   center: [params['lon'], params['lat']],
+    //   zoom: 8
+    // });
+
+
+    // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    //   mapboxgl: mapboxgl, placeholder: "Search location!" }), "top-right");
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
